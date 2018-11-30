6f48172cc79dee3d04f355a523e6ee949202a282
KelvinRod9331 <kelvinrodriguez@ac.c4q.nyc>
2018-11-29 20:57:52 -0500

    Added Fireworks effects and simulated partcile explosion

--- particles.js
+++ particles.js
@@ -1,19 +1,47 @@
-function Particle(x,y){
-    this.pos = createVector(x,y)
-    this.vel = createVector(0,0)
-    this.acc = createVector(0,0)
+function Particle(x, y, firework) {
+  this.pos = createVector(x, y);
+  this.firework = firework;
+  this.lifespan = 255;
 
-    this.applyForce = function(force){
-        acc.add(force)
+  if (this.firework) {
+    this.vel = createVector(0, random(-12, -8));
+  } else {
+    this.vel = p5.Vector.random2D();
+    this.vel.mult(random(2, 10));
+  }
+
+  this.acc = createVector(0, 0);
+
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  };
+
+  this.update = function() {
+    if (!this.firework) {
+      this.vel.mult(0.9);
+      this.lifespan -= 4;
     }
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  };
 
-    this.update = function(){
-        this.vel.add(this.acc)
-        this.pos.add(this.vel)
+  this.done = function(){
+      if(this.lifespan < 0){
+          return true   
+      } else {
+          return false
+      }
+  }
+
+  this.show = function() {
+    if (!this.firework) {
+      strokeWeight(2);
+      stroke(255, this.lifespan);
+    } else {
+      strokeWeight(4);
+      stroke(255);
     }
-
-
-    this.show = function(){
-        point()
-    }
+    point(this.pos.x, this.pos.y);
+  };
 }
